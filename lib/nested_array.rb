27c0ac6287127284c42a4d4030b40e6d98870999
Dena Neuman <denaweiss5@gmail.com>
2020-10-14 17:36:48 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,6 +33,6 @@ end
 
 def matrix_update(matrix, row, column, new_value)
 matrix[row][column][new_value]
-matrix_update[1][1][new_value]
-
+matrix_update[row][column][new_value]
+new_value="Lemons"
 end
